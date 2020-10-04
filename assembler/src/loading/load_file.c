cdded919126b78e12f9dfde5a0d3d253e840d1ff
AndreIglesias <ciglesia@student.42.fr>
2020-10-04 17:28:50 +0200

    header\0

--- assembler/src/loading/load_file.c
+++ assembler/src/loading/load_file.c
@@ -6,7 +6,7 @@
 /*   By: ciglesia <ciglesia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/15 17:45:51 by ciglesia          #+#    #+#             */
-/*   Updated: 2020/09/20 20:33:54 by ciglesia         ###   ########.fr       */
+/*   Updated: 2020/10/04 15:36:24 by ciglesia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,4 +31,5 @@ void	fill_header(char *dest, char *inst, int size, int headline)
 		dest[i++] = '\n';
 	while (inst[j] && inst[j] != '"' && i < size)
 		dest[i++] = inst[j++];
+	dest[size - 1] = '\0';
 }
