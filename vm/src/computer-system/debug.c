b577eed4585f195a0f87635cf50e80432f7d6e28
AndreIglesias <ciglesia@student.42.fr>
2020-10-13 19:40:58 +0200

    verb:dump#line

--- vm/src/computer-system/debug.c
+++ vm/src/computer-system/debug.c
@@ -6,7 +6,7 @@
 /*   By: ciglesia <ciglesia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/27 14:44:58 by ciglesia          #+#    #+#             */
-/*   Updated: 2020/09/29 20:48:21 by ciglesia         ###   ########.fr       */
+/*   Updated: 2020/10/13 19:30:23 by ciglesia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,30 +35,42 @@ int		is_pc(t_list *process, int i)
 	return (0);
 }
 
+void	dump_bytes(t_vm *vm, int i)
+{
+	if (vm->verbosity)
+	{
+		if (vm->ram.owner[i] == 0)
+			ft_printf(GRAY"%.2x "E0M, vm->ram.arena[i]);
+		else if (vm->ram.owner[i] == 1)
+			ft_printf(GREEN"%.2x "E0M, vm->ram.arena[i]);
+		else if (vm->ram.owner[i] == 2)
+			ft_printf(CYAN"%.2x "E0M, vm->ram.arena[i]);
+		else if (vm->ram.owner[i] == 3)
+			ft_printf(BLUE"%.2x "E0M, vm->ram.arena[i]);
+		else
+			ft_printf(YELLOW"%.2x "E0M, vm->ram.arena[i]);
+	}
+	else
+		ft_printf("%.2x", vm->ram.arena[i]);
+}
+
 int		print_ram(t_vm *vm)
 {
 	int i;
+	int line;
 
 	i = 0;
+	line = 0;
+	(vm->verbosity) ? ft_printf("0x%#.4x | ", line) : 0;
 	while (i < MEM_SIZE)
 	{
-		if (vm->verbosity)
-		{
-			if (vm->ram.owner[i] == 0)
-				ft_printf(GRAY"%.2x"E0M, vm->ram.arena[i]);
-			else if (vm->ram.owner[i] == 1)
-				ft_printf(GREEN"%.2x"E0M, vm->ram.arena[i]);
-			else if (vm->ram.owner[i] == 2)
-				ft_printf(CYAN"%.2x"E0M, vm->ram.arena[i]);
-			else if (vm->ram.owner[i] == 3)
-				ft_printf(BLUE"%.2x"E0M, vm->ram.arena[i]);
-			else
-				ft_printf(YELLOW"%.2x"E0M, vm->ram.arena[i]);
-		}
-		else
-			ft_printf("%.2x", vm->ram.arena[i]);
+		dump_bytes(vm, i);
 		if (i != 1 && (i + 1) % 32 == 0)
+		{
+			line += 32;
 			ft_printf("\n");
+			(vm->verbosity && i + 1 < MEM_SIZE) ? ft_printf("%0#.4x | ", line) : 0;
+		}
 		i++;
 	}
 	return (0);
