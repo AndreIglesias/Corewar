9af8257dda4f24fe06f2a635e2e4e30fc541475b
AndreIglesias <ciglesia@student.42.fr>
2020-10-15 18:49:48 +0200

    aff

--- vm/src/op/aff.c
+++ vm/src/op/aff.c
@@ -6,7 +6,7 @@
 /*   By: ciglesia <ciglesia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/28 17:03:30 by ciglesia          #+#    #+#             */
-/*   Updated: 2020/10/15 01:09:43 by ciglesia         ###   ########.fr       */
+/*   Updated: 2020/10/15 18:49:37 by ciglesia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,8 @@ void							op_aff(t_vm *vm, t_list *process)
 	if (p_acb(acb, 1) == REG && is_reg(vm, ((TP*)P->obj)->pc + 2))
 	{
 		value = ((TP*)P->obj)->reg[in_mem(vm, ((TP*)P->obj)->pc + 2) - 1];
-		ft_printf("AFF: %d : %c\n",value, value);
+		if (!vm->ncurses)
+			ft_printf("0x%.2x : %.3d :\t%c\n",value, value, value);
 	}
 	((TP*)P->obj)->pc = mem_mod(((TP*)P->obj)->pc + octal_shift(acb, 4, 1));
 }
