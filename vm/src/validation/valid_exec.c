571b4aaf9123595b173ee8f060c12c6460ca25e9
AndreIglesias <ciglesia@student.42.fr>
2020-10-20 12:26:07 +0200

    close() if EXIT_FAILURE (vm validation)

--- vm/src/validation/valid_exec.c
+++ vm/src/validation/valid_exec.c
@@ -6,7 +6,7 @@
 /*   By: ciglesia <ciglesia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/25 19:42:06 by ciglesia          #+#    #+#             */
-/*   Updated: 2020/10/16 22:55:53 by ciglesia         ###   ########.fr       */
+/*   Updated: 2020/10/20 12:25:05 by ciglesia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ int		valid_magic(char *file)
 
 	if ((fd = open(file, O_RDONLY)) < 0)
 	{
-		close(fd);
 		ft_printf_fd(2, ERROR": %s: Couldn't open file.\n", file);
 		return (EXIT_FAILURE);
 	}
@@ -27,6 +26,7 @@ int		valid_magic(char *file)
 	read(fd, &exec_magic[2], 1);
 	read(fd, &exec_magic[1], 1);
 	read(fd, &exec_magic[0], 1);
+	close(fd);
 	if (*(unsigned int*)exec_magic != COREWAR_EXEC_MAGIC)
 	{
 		ft_printf_fd(2, ERROR": %s: Improper exec_magic\n", file);
@@ -43,7 +43,6 @@ int		diff_size(char *file, long size)
 	ft_memset(prog_size, 0, 8);
 	if ((fd = open(file, O_RDONLY)) < 0)
 	{
-		close(fd);
 		ft_printf_fd(2, ERROR": %s: Couldn't open file.\n", file);
 		return (EXIT_FAILURE);
 	}
@@ -69,13 +68,12 @@ int		valid_header(int fd, char *file)
 	int		size;
 
 	size = lseek(fd, 0, SEEK_END) - sizeof(t_header);
+	close(fd);
 	if (size > CHAMP_MAX_SIZE)
 	{
-		close(fd);
 		ft_printf_fd(2, ERROR": %s: Improper champion size(%d)\n", file, size);
 		return (EXIT_FAILURE);
 	}
-	close(fd);
 	if (diff_size(file, size) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
 	if (valid_magic(file) == EXIT_FAILURE)
